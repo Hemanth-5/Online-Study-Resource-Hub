8cb149936975d7846360b508d15c40d9a696aedd
Hemanth-5 <22z225@psgtech.ac.in>
2024-09-28 17:07:33 +0530

    change in auth

--- server/config/auth.js
+++ server/config/auth.js
@@ -12,7 +12,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID.toString(),
       clientSecret: process.env.GOOGLE_CLIENT_SECRET.toString(),
-      callbackURL: "/api/auth/google/callback",
+      callbackURL: process.env.GOOGLE_CALLBACK_URL.toString(),
     },
     handleGoogleLogin
   )
