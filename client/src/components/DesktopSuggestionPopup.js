5991bcbd371cc07741b3775b2374aab9a187910f
Hemanth-5 <22z225@psgtech.ac.in>
2024-10-01 00:12:45 +0530

    Refactor DesktopSuggestionPopup component and update message for better user experience

--- client/src/components/DesktopSuggestionPopup.js
+++ client/src/components/DesktopSuggestionPopup.js
@@ -11,8 +11,10 @@ const DesktopSuggestionPopup = ({ show, onClose }) => {
       <div className="desktop-popup-container">
         <h2>Switch to Desktop View</h2>
         <p>
-          For a better experience, we recommend using the desktop version of the
-          website.
+          {/* For a better experience, we recommend using the desktop version of the
+          website. */}
+          The website is still under development. Please use the desktop version
+          for a better experience. Thank you! 😊
         </p>
         <div className="desktop-popup-actions">
           <button className="desktop-popup-button" onClick={onClose}>
