20703d41a86724ee9010693614451d328910cb4e
Hemanth-5 <22z225@psgtech.ac.in>
2024-10-02 16:17:31 +0530

    change in server

--- server/index.js
+++ server/index.js
@@ -21,6 +21,8 @@ dotenv.config();
 // App Config
 const app = express();
 
+console.log(process.env.FRONTEND_URL.toString());
+
 // Inbuilt Middleware
 app.use(express.json());
 app.use(
@@ -33,6 +35,19 @@ app.use(
 app.use(express.urlencoded({ extended: false }));
 
 app.use(passport.initialize());
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    process.env.VERCEL_FRONTEND.toString()
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 // API Endpoints
 app.use("/api/auth", authRoutes);
